5c70466eaa7854cb9a20a0129a8c48db75239866
Student <student@Makerss-MacBook-Pro-2.local>
2019-07-01 15:09:41 +0100

    adding maximum balance

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,9 +1,16 @@
 class Oystercard
 
+MAXIMUM_BALANCE = 90 
+
 attr_reader :balance
 
-  def initialize
-    @balance = 0
+  def initialize(balance=0)
+    @balance = balance
   end
+
+  def top_up(amount)
+    raise "you don't want to loose that much money"  if @balance + amount > MAXIMUM_BALANCE
+    @balance += amount
+  end 
   
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -3,6 +3,17 @@ require 'Oystercard'
 describe Oystercard do
   subject(:oystercard) { described_class.new }
   it "should have a balance" do
-    expect(oystercard.balance).to eq(0)
+    expect(oystercard.balance).to eq(0)  
   end
-end
+  it 'can top up the balance' do
+    expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
+    # change if rspec fails to one line 
+  end
+
+  it "raises an error" do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    oystercard.top_up maximum_balance
+    expect{oystercard.top_up(1)}.to raise_error "you don't want to loose that much money" 
+  end 
+end 
+
