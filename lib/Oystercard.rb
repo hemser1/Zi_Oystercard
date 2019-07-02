812bbae22b3250d21f2931a0bc81645baa0acb35
Student <ziwei.polaris@gmail.com>
2019-07-02 13:54:37 +0100

    update

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,36 +1,44 @@
+class Station
+
+end
+
 class Oystercard
 
 MAXIMUM_BALANCE = 90
 MINIMUM_CHARGE = 1
 COST = MINIMUM_CHARGE
 
-attr_reader :balance, :in_journey
+attr_reader :balance, :entry_station
 
   def initialize(balance=0)
     @balance = balance
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
-    raise "you don't want to loose that much money"  if @balance + amount > MAXIMUM_BALANCE
+    raise "you don't want to lose that much money"  if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station = "default")
     raise "Minimum balance is 1£" if @balance < MINIMUM_CHARGE
     @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
     @in_journey = false
+    @entry_station = nil
     deduct(COST)
   end
 
   def in_journey?
-    @in_journey
+    @entry_station
+    #same as !!@entry_station
   end
 
-private 
+private
   def deduct(amount)
     @balance -= amount
   end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,25 +2,28 @@ require 'Oystercard'
 
 describe Oystercard do
   subject(:oystercard) { described_class.new }
+  let(:station) { double :station}
   it "should have a balance" do
     expect(oystercard.balance).to eq(0)
   end
   it 'can top up the balance' do
     expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
-    # change if rspec fails to one line
+  # change if rspec fails to one line
   end
-  
+
   it "raises an error" do
     maximum_balance = Oystercard::MAXIMUM_BALANCE
     oystercard.top_up maximum_balance
     expect{oystercard.top_up(1)}.to raise_error "you don't want to loose that much money"
   end
 
-  it "raises and error" do 
+  it "raises and error" do
     minimum_balance = Oystercard::MINIMUM_CHARGE
     oystercard.top_up minimum_balance-0.1
-    expect{oystercard.touch_in}.to raise_error "Minimum balance is 1£" 
-  end 
+    expect{oystercard.touch_in}.to raise_error "Minimum balance is 1£"
+  end
+
+
 
   # it "deducts money from the balance" do
   #   oystercard.top_up(20)
@@ -44,10 +47,15 @@ describe Oystercard do
       expect(oystercard).not_to be_in_journey
     end
 
-    it "deduces money" do 
+    it "deduces money" do
       cost = Oystercard::COST
-      expect{ oystercard.touch_out}.to change{ oystercard.balance }.by -cost 
-    end 
+      expect{ oystercard.touch_out}.to change{ oystercard.balance }.by -cost
+    end
+
+    it "will remember the touch in station" do
+      oystercard.touch_in(station)
+      expect(oystercard.entry_station).to eq station
+    end
 
   end
 end
